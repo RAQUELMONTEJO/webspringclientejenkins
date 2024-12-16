33253f3f9e3c09e435a24104f4757636e8aaabc4
raque <raque@host.docker.internal>
2024-12-16 10:51:37 +0100

    cliente spring para tarea jenkins

---
+++ webcliente/src/main/java/com/arquitecturajava/webcliente/ClienteRestPersona.java
@@ -0,0 +1,23 @@
+package com.arquitecturajava.webcliente;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.aquitecturajava.webspring.dtos.PersonaDto;
+
+@RestController
+public class ClienteRestPersona {
+	@RequestMapping("/personascliente")
+	public List<PersonaDto> listPersonas() {
+		RestTemplate plantilla = new RestTemplate();
+		ResponseEntity<PersonaDto[]> lista = plantilla.getForEntity("http://localhost:8083/personas", PersonaDto[].class);
+		PersonaDto[] nuevaLista = lista.getBody();
+		return Arrays.asList(nuevaLista);
+	}
+
+}
